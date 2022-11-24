9f05080b8f010352b360c6c71b4bd7106245b4e7
dennis <dennis.brokking@gmail.com>
2022-11-24 17:08:13 +0100

    Added hardcoded wallet

--- Eksamen-SoftwareDesign/Product.cs
+++ Eksamen-SoftwareDesign/Product.cs
@@ -10,32 +10,25 @@ namespace NettButikk
     internal class Product
     {
 
-
         public void DisplayItemStol()
         {
+            
             WriteLine("==============================================");
-            WriteLine("STOLER");
-            WebShopDataBase db = new();
-
-            //db.CreateDbAndTable();
-
-            int firstProductId = db.InsertProduct("Elkjøp Sofa", "100");
-            int secondProductId = db.InsertProduct("Ikea Gamingbord", "50");
-
-            string firstProductCreated = db.ReadProductName(firstProductId);
-            string secondProductCreated = db.ReadProductName(secondProductId);
-
-            Console.WriteLine($"First product:  '{firstProductCreated}'");
-            Console.WriteLine($"Second product: '{secondProductCreated}'");
+            WriteLine("PRODUKTER");
+            sellItem("a) Elkjøp Gamingstol", 2000, "Flott rød og svar gamingstol fra elkjøp!");
+            sellItem("b) IKEA Kontorstol", 1100, "Helt vanlig svart kontorstol fra IKEA som funker flott for skolen!");
             WriteLine("==============================================");
         }
 
     
         public void DisplayItemBord()
         {
+
+
             WriteLine("==============================================");
             WriteLine("PRODUKTER");
-            sellItem("IKEA Skrivebord", 150, "Den er litt whack lowkey men den funker");
+            sellItem("a) IKEA Skrivebord", 150, "Den er litt whack lowkey men den funker");
+            sellItem("b) JYSK Spisebord", 500, "Den er flott for hele familien!");
             WriteLine("==============================================");
         }
 
@@ -43,7 +36,8 @@ namespace NettButikk
         {
             WriteLine("==============================================");
             WriteLine("PRODUKTER");
-            sellItem("PAX skap", 1550, "Stor å fin garderobeskap med skyvedører");
+            sellItem("a) PAX skap", 1550, "Stor å fin garderobeskap med skyvedører");
+            sellItem("b) IKEA Skap", 750, "Stor dobbedør skap for alle dine flotte klær");
             WriteLine("==============================================");
 
         }
@@ -52,7 +46,8 @@ namespace NettButikk
         {
             WriteLine("==============================================");
             WriteLine("PRODUKTER");
-            sellItem("Scandinavian Sofa", 5600, "Behagelig 3 seters sofa laget med ekte skandinavisk kamel");
+            sellItem("a) Scandinavian Sofa", 5600, "Behagelig 3 seters sofa laget med ekte skandinavisk kamel");
+            sellItem("b) IKEA Sofa", 3500, "Flott 5 personers sofa som er laget av flott skinn");
             WriteLine("==============================================");
         }
 
@@ -61,8 +56,6 @@ namespace NettButikk
             WriteLine($"Navn: {itemName}\nPris: {cost} \nBeskrivelse: {beskrivelse}");
 
         }
-
-
 
     }
 }
--- Eksamen-SoftwareDesign/Shop.cs
+++ Eksamen-SoftwareDesign/Shop.cs
@@ -10,6 +10,7 @@ namespace NettButikk
 {
     class Shop
     {
+        public int totalCost;
         public void run()
         {
             // Shop logic
@@ -30,29 +31,121 @@ namespace NettButikk
 
         private void AskForCategory()
         {
+
             Product product = new Product();
-           
             WriteLine("Vennligst velg et kategori mellom a-d");
             string kundeKategori = ReadLine();
             if(kundeKategori == "a")
             {
                 product.DisplayItemStol();
-            } if(kundeKategori == "b")
+                buyStol();
+            } 
+            if(kundeKategori == "b")
             {
                 product.DisplayItemBord();
-            } if(kundeKategori == "c")
+                buyTable();
+            } 
+            if(kundeKategori == "c")
             {
                 product.DisplayItemGarderobeSkap();
-            } if (kundeKategori == "d")
+                buySkap();
+            } 
+            if (kundeKategori == "d")
             {
                 product.DisplayItemSofa();
+                buySofa();
             }
 
         }
 
-        private void DisplayOrderTotal()
+        private void buyStol()
         {
+            Product product = new Product();
+            String valgStol = ReadLine();
 
+            if (valgStol == "a")
+            {
+                totalCost += 2000;
+                WriteLine("2000$ Has been added to the total cost. Total cost is now: " + totalCost + "$");
+                displayCheckout();
+            }
+            if (valgStol == "b")
+            {
+                totalCost += 1100;
+                WriteLine("1100$ Has been added to the total cost.");
+                displayCheckout();
+            }
+        }
+
+        private void buyTable()
+        {
+            Product product = new Product();
+            String valgBord = ReadLine();
+            
+            if (valgBord == "a")
+            {
+                totalCost += 150;
+                WriteLine("150$ Has been added to the total cost. Total cost is now: " + totalCost + "$");
+                displayCheckout();
+            }
+            if (valgBord == "b")
+            {
+                totalCost += 500;
+                WriteLine("500$ Has been added to the total cost.");
+                displayCheckout();
+            }
+        }
+
+        private void buySkap()
+        {
+            Product product = new Product();
+            String valgSkap = ReadLine();
+
+            if (valgSkap == "a")
+            {
+                totalCost += 1550;
+                WriteLine("1550$ Has been added to the total cost. Total cost is now: " + totalCost + "$");
+                displayCheckout();
+            }
+            if (valgSkap == "b")
+            {
+                totalCost += 750;
+                WriteLine("750$ Has been added to the total cost.");
+                displayCheckout();
+            }
+        }
+
+        private void buySofa()
+        {
+            Product product = new Product();
+            String valgSofa = ReadLine();
+
+            if (valgSofa == "a")
+            {
+                totalCost += 5600;
+                WriteLine("5600$ Has been added to the total cost. Total cost is now: " + totalCost + "$");
+                displayCheckout();
+            }
+            if (valgSofa == "b")
+            {
+                totalCost += 3500;
+                WriteLine("3500$ Has been added to the total cost.");
+                displayCheckout();
+            }
+        }
+
+        private void displayCheckout()
+        {
+            WriteLine("Total cost is now: " + totalCost + "$");
+            WriteLine("Do you want to contine shopping? y/n");
+            String continueYorN = ReadLine();
+            if (continueYorN == "y"){
+                run();
+
+            } if (continueYorN == "n")
+            {
+                DisplayOutro();
+            }
         }
 
         private void DisplayCategory()
@@ -66,10 +159,11 @@ namespace NettButikk
         
         private void DisplayOutro()
         {
+            WriteLine("Your totale came out to be: " + totalCost);
             WriteLine("Thanks for shopping!");
             WriteLine("Press any key to exit...");
             ReadKey();
         }
-        
+
     }
 }
