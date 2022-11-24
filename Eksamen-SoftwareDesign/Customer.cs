43593e10866e862910060a4426f4f9e8a07e0b31
RojhatAlg <roal005@student.kristiania.no>
2022-11-25 00:55:23 +0100

    Sorted out classes. 12 total classes.

--- Eksamen-SoftwareDesign/Customer.cs
+++ Eksamen-SoftwareDesign/Customer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace NettButikk
 {
@@ -10,6 +11,10 @@ namespace NettButikk
     {
         private string customerName;
         private string customerWallet;
+
+        public Customer()
+        {
+        }
 
         public Customer(string customerName, string customerWallet)
         {
@@ -29,5 +34,32 @@ namespace NettButikk
             set { customerWallet = value; }
         }
 
+        public void DisplayCustomers()
+        {
+            WriteLine("==============================================");
+            WriteLine("CUSTOMERS");
+            CreateCustomer();
+            WriteLine("==============================================");
+
+        }
+
+        private void CreateCustomer()
+        {
+            CustomerDataBase db_customer = new();
+            db_customer.CreateDbAndTable();
+
+            int firstCustomerId = db_customer.InsertCustomer("Mohammed", "2500");
+            int secondCustomerId = db_customer.InsertCustomer("Jonas", "1500");
+
+            Customer firstCustomerCreated = db_customer.ReadCustomer(firstCustomerId);
+            Customer secondCustomerCreated = db_customer.ReadCustomer(secondCustomerId);
+
+            Console.WriteLine($"First Customer:  '{firstCustomerCreated.CustomerName}'" + $" '{firstCustomerCreated.CustomerWallet}' kroner ");
+            Console.WriteLine($"Secound Customer:  '{secondCustomerCreated.CustomerName}'" + $" '{secondCustomerCreated.CustomerWallet}' kroner ");
+        }
+
+
     }
+
+    
 }
--- Eksamen-SoftwareDesign/Shop.cs
+++ Eksamen-SoftwareDesign/Shop.cs
@@ -12,9 +12,12 @@ namespace NettButikk
     {
         public void run()
         {
+            Customer c = new Customer();
+            Categories categories = new Categories();
             // Shop logic
+            c.DisplayCustomers();
             DisplayIntro();
-            AskForCategory();
+            categories.AskForCategory();
             DisplayOutro();
         }
 
@@ -27,151 +30,6 @@ namespace NettButikk
         }
 
 
-
-        private void AskForCategory()
-        {
-
-            DisplayProduct product = new DisplayProduct();
-            WriteLine("Vennligst velg et kategori mellom a-d");
-            string kundeKategori = ReadLine();
-            if(kundeKategori == "a")
-            {
-                product.DisplayItemStol();
-                buyStol();
-            } 
-            if(kundeKategori == "b")
-            {
-                product.DisplayItemBord();
-                buyTable();
-            } 
-            if(kundeKategori == "c")
-            {
-                product.DisplayItemGarderobeSkap();
-                buySkap();
-            } 
-            if (kundeKategori == "d")
-            {
-                product.DisplayItemSofa();
-                buySofa();
-            }
-
-        }
-
-        private void buyStol()
-        {
-            Product product = new Product();
-            String valgStol = ReadLine();
-
-            if (valgStol == "a")
-            {
-                Money.totalCost += 2000;
-                WriteLine("2000$ Has been added to the total cost. Total cost is now: " + Money.totalCost + "$");
-                displayCheckout();
-            }
-            if (valgStol == "b")
-            {
-                Money.totalCost += 1100;
-                WriteLine("1100$ Has been added to the total cost.");
-                displayCheckout();
-            }
-            if (valgStol == "c")
-            {
-                Money.totalCost += 20;
-                WriteLine("20$ Has been added to the total cost.");
-                displayCheckout();
-            }
-        }
-
-        private void buyTable()
-        {
-            Product product = new Product();
-            String valgBord = ReadLine();
-            
-            if (valgBord == "a")
-            {
-                Money.totalCost += 1200;
-                WriteLine("1200$ Has been added to the total cost. Total cost is now: " + Money.totalCost + "$");
-                displayCheckout();
-            }
-            if (valgBord == "b")
-            {
-                Money.totalCost += 380;
-                WriteLine("380$ Has been added to the total cost.");
-                displayCheckout();
-            }
-
-            if (valgBord == "c")
-            {
-                Money.totalCost += 750;
-                WriteLine("750$ Has been added to the total cost.");
-                displayCheckout();
-            }
-        }
-
-        private void buySkap()
-        {
-            Product product = new Product();
-            String valgSkap = ReadLine();
-
-            if (valgSkap == "a")
-            {
-                Money.totalCost += 2230;
-                WriteLine("2230$ Has been added to the total cost. Total cost is now: " + Money.totalCost + "$");
-                displayCheckout();
-            }
-            if (valgSkap == "b")
-            {
-                Money.totalCost += 630;
-                WriteLine("630$ Has been added to the total cost.");
-                displayCheckout();
-            }
-        }
-
-        private void buySofa()
-        {
-            Product product = new Product();
-            String valgSofa = ReadLine();
-
-            if (valgSofa == "a")
-            {
-                Money.totalCost += 3500;
-                WriteLine("3500$ Has been added to the total cost. Total cost is now: " + Money.totalCost + "$");
-                displayCheckout();
-            }
-            if (valgSofa == "b")
-            {
-                Money.totalCost += 5000;
-                WriteLine("5000$ Has been added to the total cost.");
-                displayCheckout();
-            }
-            if (valgSofa == "c")
-            {
-                Money.totalCost += 3350;
-                WriteLine("3350$ Has been added to the total cost.");
-                displayCheckout();
-            }
-            if (valgSofa == "d")
-            {
-                Money.totalCost += 7850;
-                WriteLine("7850$ Has been added to the total cost.");
-                displayCheckout();
-            }
-        }
-
-        private void displayCheckout()
-        {
-            WriteLine("Total cost is now: " + Money.totalCost + "$");
-            WriteLine("Do you want to contine shopping? y/n");
-            String continueYorN = ReadLine();
-            if (continueYorN == "y"){
-                run();
-
-            } if (continueYorN == "n")
-            {
-                DisplayOutro();
-            }
-        }
-
         private void DisplayCategory()
         {
             WriteLine("a: Stoler");
@@ -181,7 +39,7 @@ namespace NettButikk
 
         }
         
-        private void DisplayOutro()
+        public void DisplayOutro()
         {
             WriteLine("Your totale came out to be: " + Money.totalCost);
             WriteLine("Thanks for shopping! :)");
@@ -189,5 +47,164 @@ namespace NettButikk
             ReadKey();
         }
 
+    }
+
+    internal class Categories
+    {
+        public void AskForCategory()
+        {
+
+            DisplayProduct product = new DisplayProduct();
+            WriteLine("Vennligst velg et kategori mellom a-d");
+            string kundeKategori = ReadLine();
+            Buy buy = new Buy();
+            if (kundeKategori == "a")
+            {
+                product.DisplayItemStol();
+                buy.buyStol();
+            }
+            if (kundeKategori == "b")
+            {
+                product.DisplayItemBord();
+                buy.buyTable();
+            }
+            if (kundeKategori == "c")
+            {
+                product.DisplayItemGarderobeSkap();
+                buy.buySkap();
+            }
+            if (kundeKategori == "d")
+            {
+                product.DisplayItemSofa();
+                buy.buySofa();
+            }
+
+        }
+    }
+
+    internal class Buy
+    {
+
+        ShoppingCart cart = new ShoppingCart();
+        public void buyStol()
+        {
+            Product product = new Product();
+            String valgStol = ReadLine();
+
+            if (valgStol == "a")
+            {
+                Money.totalCost += 2000;
+                WriteLine("2000$ Has been added to the total cost. Total cost is now: " + Money.totalCost + "$");
+                cart.displayCheckout();
+            }
+            if (valgStol == "b")
+            {
+                Money.totalCost += 1100;
+                WriteLine("1100$ Has been added to the total cost.");
+                cart.displayCheckout();
+            }
+            if (valgStol == "c")
+            {
+                Money.totalCost += 20;
+                WriteLine("20$ Has been added to the total cost.");
+                cart.displayCheckout();
+            }
+        }
+
+        public void buyTable()
+        {
+            Product product = new Product();
+            String valgBord = ReadLine();
+
+            if (valgBord == "a")
+            {
+                Money.totalCost += 1200;
+                WriteLine("1200$ Has been added to the total cost. Total cost is now: " + Money.totalCost + "$");
+                cart.displayCheckout();
+            }
+            if (valgBord == "b")
+            {
+                Money.totalCost += 380;
+                WriteLine("380$ Has been added to the total cost.");
+                cart.displayCheckout();
+            }
+
+            if (valgBord == "c")
+            {
+                Money.totalCost += 750;
+                WriteLine("750$ Has been added to the total cost.");
+                cart.displayCheckout();
+            }
+        }
+
+        public void buySkap()
+        {
+            Product product = new Product();
+            String valgSkap = ReadLine();
+
+            if (valgSkap == "a")
+            {
+                Money.totalCost += 2230;
+                WriteLine("2230$ Has been added to the total cost. Total cost is now: " + Money.totalCost + "$");
+                cart.displayCheckout();
+            }
+            if (valgSkap == "b")
+            {
+                Money.totalCost += 630;
+                WriteLine("630$ Has been added to the total cost.");
+                cart.displayCheckout();
+            }
+        }
+
+        public void buySofa()
+        {
+            Product product = new Product();
+            String valgSofa = ReadLine();
+
+            if (valgSofa == "a")
+            {
+                Money.totalCost += 3500;
+                WriteLine("3500$ Has been added to the total cost. Total cost is now: " + Money.totalCost + "$");
+                cart.displayCheckout();
+            }
+            if (valgSofa == "b")
+            {
+                Money.totalCost += 5000;
+                WriteLine("5000$ Has been added to the total cost.");
+                cart.displayCheckout();
+            }
+            if (valgSofa == "c")
+            {
+                Money.totalCost += 3350;
+                WriteLine("3350$ Has been added to the total cost.");
+                cart.displayCheckout();
+            }
+            if (valgSofa == "d")
+            {
+                Money.totalCost += 7850;
+                WriteLine("7850$ Has been added to the total cost.");
+                cart.displayCheckout();
+            }
+        }
+    }
+
+    internal class ShoppingCart
+    {
+        public void displayCheckout()
+        {
+            WriteLine("Total cost is now: " + Money.totalCost + "$");
+            WriteLine("Do you want to contine shopping? y/n");
+            String continueYorN = ReadLine();
+            Shop shop = new Shop();
+            if (continueYorN == "y")
+            {
+                shop.run();
+
+            }
+            if (continueYorN == "n")
+            {
+                shop.DisplayOutro();
+            }
+        }
     }
 }
